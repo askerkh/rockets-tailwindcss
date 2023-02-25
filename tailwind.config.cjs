4251e9d3e933130fb27906d342539986811e5c68
Asker Khuzhokov <a7ker.kh@gmail.com>
2023-02-25 21:18:42 +0300

    Added new section and footer

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,11 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'widescreen': { 'raw': '(min-aspect-ratio: 3/2)' },
+        'tallscreen': { 'raw': '(min-aspect-ratio: 1/2)' },
+      }
+    },
   },
 }
